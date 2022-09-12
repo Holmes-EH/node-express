a49e1b6e851a351b0b4aabe9bb4c2438d9c77074
Holmes-EH <holmes.samuel@yahoo.fr>
2022-09-12 16:58:02 +0200

    move dataSource to utils

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+const DataSource = require('typeorm').DataSource
+
+module.exports = {
+	dataSource: new DataSource({
+		type: 'sqlite',
+		database: './wildersdb.sqlite',
+		synchronize: true,
+		entities: [require('./entity/Wilder')],
+	}),
+}
