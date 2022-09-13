e2d1b29021519c1687ba92700f65a4416a273753
Holmes-EH <holmes.samuel@yahoo.fr>
2022-09-13 12:34:35 +0200

    add crud to wilder controller

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -1,6 +1,67 @@
+const dataSource = require('../utils').dataSource
+const Wilder = require('../entity/Wilder')
+
 module.exports = {
-	create: (req, res) => {
-		console.log(req.body)
-		res.send('Got request from client')
+	create: async (req, res) => {
+		try {
+			await dataSource.getRepository(Wilder).save(req.body)
+			res.status(201).send('Wilder Created !')
+		} catch (error) {
+			console.error('Error ->', error)
+			res.status(500).send(`Error while creating Wilder : ${error}`)
+		}
+	},
+	findAll: async (req, res) => {
+		try {
+			const wilders = await dataSource.getRepository(Wilder).find()
+			if (wilders.length > 0) {
+				res.status(200).send(wilders)
+			} else {
+				res.status(404).send('No Wilders found ...')
+			}
+		} catch (error) {
+			console.error(error)
+			res.status(500).send(`Error fetching Wilders : ${error}`)
+		}
+	},
+	update: async (req, res) => {
+		try {
+			const { id, name } = req.body
+			const wilderToUpdate = await dataSource
+				.getRepository(Wilder)
+				.findOneBy({ id: id })
+			if (wilderToUpdate === null) {
+				res.status(404).send('Wilder Not found')
+			} else {
+				try {
+					await dataSource
+						.getRepository(Wilder)
+						.update(id, { name: name })
+					res.status(200).send('Wilder Updated Successfully')
+				} catch (error) {
+					res.status(500).send(
+						`An error occured updating Wilder : ${error}`
+					)
+				}
+			}
+		} catch (error) {
+			console.error(error)
+			res.status(500).send(`Error while updating Wilder : ${error}`)
+		}
+	},
+	delete: async (req, res) => {
+		try {
+			const { id } = req.body
+			const deletedWilder = await dataSource
+				.getRepository(Wilder)
+				.delete(id)
+			console.log(deletedWilder)
+			if (deletedWilder.affected === 0)
+				res.status(404).send('Could not delete. Wilder not found')
+			res.status(202).send('Wilder deleted successfully')
+		} catch (error) {
+			console.error(error)
+			res.status(500).send(`Error while deleting Wilder : ${error}`)
+		}
 	},
 }
