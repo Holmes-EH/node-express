46188368f44536a0f0fac478455eca55d7ddd5c3
Holmes-EH <holmes.samuel@yahoo.fr>
2022-09-19 09:41:57 +0200

    use cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const Wilder = require('./entity/Wilder')
+const cors = require('cors')
 const dataSource = require('./utils').dataSource
 const wilderController = require('./controller/wilder')
 const skillController = require('./controller/skill')
+const gradeController = require('./controller/grade')
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
@@ -18,7 +20,7 @@ app.post('/api/wilders', wilderController.create)
 app.get('/api/wilders', wilderController.findAll)
 app.put('/api/wilders', wilderController.update)
 
-app.put('/api/wilders/addSkill', wilderController.addSkill)
+app.post('/api/wilders/addSkill', gradeController.noteSkill)
 
 app.delete('/api/wilders', wilderController.delete)
 
@@ -27,6 +29,10 @@ app.post('/api/skills', skillController.create)
 app.get('/api/skills', skillController.findAll)
 app.put('/api/skills', skillController.update)
 app.delete('/api/skills', skillController.delete)
+
+app.use((req, res, next) => {
+	res.status(404).send('Route does not exist...')
+})
 
 const start = async () => {
 	await dataSource.initialize()
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,11 @@ module.exports = {
 		type: 'sqlite',
 		database: './wildersdb.sqlite',
 		synchronize: true,
-		entities: [require('./entity/Wilder'), require('./entity/Skill')],
+		entities: [
+			require('./entity/Wilder'),
+			require('./entity/Skill'),
+			require('./entity/Grades'),
+		],
 		logging: ['error'],
 	}),
 }
